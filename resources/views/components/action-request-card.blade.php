3dcac9b4ffdc46c702d6afb5fd05f9dd879c0ade
s1136647 <s1136647@student.hsleiden.nl>
2023-06-01 14:57:22 +0200

    buttons

--- resources/views/components/action-request-card.blade.php
+++ resources/views/components/action-request-card.blade.php
@@ -7,12 +7,8 @@
             <li><p>Naam dier: {{$careRequests->advertisement_name_animal}}</p></li>
             <li><p>Soort dier: {{$careRequests->advertisement_animal}}</p></li>
             <li style="margin-top: 20px;"><a href="/account/{{$careRequests->care_taker_id}}/bekijken" style="color: rgb(63, 58, 58);">Bekijk profiel</a></li>
-            {{-- <li><p>Prijs: € {{$careRequests->advertisement_price}}/ dag</p></li>
-            <li><p>Van: {{$careRequests->advertisement_date_start}}</p></li>
-            <li><p>Tot: {{$careRequests->advertisement_date_end}}</p></li> --}}
-            <!-- <li><p>Meer informatie</p></li> -->
         </div>
-        <section class="action-buttons">
+        <section class="action-buttons" style="margin-right: 50px">
             <div>
                 <form method="POST" action="/advertenties/{{$careRequests->id}}/accepteren">
                     @csrf
--- resources/views/components/advertisement-card.blade.php
+++ resources/views/components/advertisement-card.blade.php
@@ -9,11 +9,10 @@
             <li><p>€{{$advertisements->price}} / dag</p></li>
             <li><p>Van: {{$advertisements->date_start}}</p></li>
             <li><p>Tot: {{$advertisements->date_end}}</p></li>
-            <!-- <li><p>Meer informatie</p></li> -->
         </div>
         <section class="action-buttons">
             <div>
-                <a href="/advertenties/{{$advertisements->id}}/bewerken" class="action-btn" href="" style="background-color: #ffffff;"><i class="fa-solid fa-pen-to-square"></i></a> 
+                <a href="/advertenties/{{$advertisements->id}}/bewerken" class="action-btn" href="" style="background-color: #ffffff; margin-top: 10px"><i class="fa-solid fa-pen-to-square"></i></a> 
             </div>
             <div>
                 <form method="POST" action="/advertisements/{{$advertisements->id}}">

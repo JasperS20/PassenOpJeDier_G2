9339f2708186bcce8aaa97f90075a8133b14cd87
s1136647 <s1136647@student.hsleiden.nl>
2023-06-02 14:31:45 +0200

    commit

--- resources/views/admin/advertisements.blade.php
+++ resources/views/admin/advertisements.blade.php
@@ -2,41 +2,43 @@
 
 @section('admin-table')
 <section>
-    <table class="user-table">
-        <thead>
-            <tr>
-                <th class="table-header">Advertentie ID</th>
-                <th class="table-header">Gebruiker ID</th>
-                <th class="table-header">Gebruikersnaam</th>
-                <th class="table-header">Naam dier</th>
-                <th class="table-header">Soort dier</th>
-                <th class="table-header">prijs</th>
-                <th class="table-header">Datum start</th>
-                <th class="table-header">Datum eind</th>
-                <th class="table-header">Acties</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($advertisements as $advertisements)
+    <div class="table-wrapper">
+        <table class="user-table">
+            <thead>
                 <tr>
-                    <td>{{ $advertisements->id }}</td>
-                    <td>{{ $advertisements->user_id }}</td>
-                    <td>{{ $advertisements->user_name }}</td>
-                    <td>{{ $advertisements->name }}</td>
-                    <td>{{ $advertisements->animal }}</td>
-                    <td>{{ $advertisements->price }}</td>
-                    <td>{{ $advertisements->date_start }}</td>
-                    <td>{{ $advertisements->date_end }}</td>
-                    <td>
-                        <form method="POST" action="{{ route('advertisements.delete', ['id' => $advertisements->id]) }}">
-                            @csrf
-                            @method('PUT')
-                            <input type="submit" value="Verwijderen" style="background-color: red;">
-                        </form>
-                    </td>
+                    <th class="table-header">Advertentie ID</th>
+                    <th class="table-header">Gebruiker ID</th>
+                    <th class="table-header">Gebruikersnaam</th>
+                    <th class="table-header">Naam dier</th>
+                    <th class="table-header">Soort dier</th>
+                    <th class="table-header">prijs</th>
+                    <th class="table-header">Datum start</th>
+                    <th class="table-header">Datum eind</th>
+                    <th class="table-header">Acties</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($advertisements as $advertisements)
+                    <tr>
+                        <td>{{ $advertisements->id }}</td>
+                        <td>{{ $advertisements->user_id }}</td>
+                        <td>{{ $advertisements->user_name }}</td>
+                        <td>{{ $advertisements->name }}</td>
+                        <td>{{ $advertisements->animal }}</td>
+                        <td>{{ $advertisements->price }}</td>
+                        <td>{{ $advertisements->date_start }}</td>
+                        <td>{{ $advertisements->date_end }}</td>
+                        <td>
+                            <form method="POST" action="{{ route('advertisements.delete', ['id' => $advertisements->id]) }}">
+                                @csrf
+                                @method('PUT')
+                                <input type="submit" value="Verwijderen" style="background-color: red;">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </section>
 @endsection('admin-table')
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1 style="margin-left: 50px; font-size: 30px; color: #58A746">Welkom {{ Auth::user()->name }} (Admin)</h1>
-    <ul style="margin: 50px">
+    <ul class="admin-links" style="margin: 50px">
         <a href="{{ route('admin.users') }}" class="admin-tabel-btn">Gebruikers tabel</a>
         <a href="{{ route('admin.advertisements') }}" class="admin-tabel-btn">Advertenties tabel</a>
         <a href="{{ route('admin.requests') }}" class="admin-tabel-btn">Aanvragen tabel</a>
